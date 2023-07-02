ff87667f5b5a6a9f8780893d22b863a58db875a1
Oscar <oscar.cn1@gmail.com>
2023-07-02 11:25:42 -0600

    Cache CRUD

--- src/main/java/com/nuuptech/infinispan/demo/controller/UserController.java
+++ src/main/java/com/nuuptech/infinispan/demo/controller/UserController.java
@@ -8,54 +8,65 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    private final RemoteCacheManager cacheManager;
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private RemoteCache<String, String> cache;
+    private ObjectMapper objectMapper;
 
     @Autowired
     public UserController(RemoteCacheManager cacheManager) {
-        this.cacheManager = cacheManager;
+        this.cache = cacheManager.getCache("usuarios");
+        this.objectMapper = new ObjectMapper();
     }
 
     @PostMapping("/create")
     public ResponseEntity<Void> createUser(@RequestBody User user) throws JsonProcessingException {
-        RemoteCache<String, String> cache = cacheManager.getCache("usuarios");
         String userJson = objectMapper.writeValueAsString(user);
-        log.info("Create user: {}", userJson);
+        log.debug("Create user: {}", userJson);
         cache.put(user.getId(), userJson);
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<User> getUsuer(@PathVariable String id) throws JsonProcessingException {
-        RemoteCache<String, String> cache = cacheManager.getCache("usuarios");
         String userJson = cache.get(id);
-        log.info("Get user: {}", userJson);
+        log.debug("Get user: {}", userJson);
         User user = objectMapper.readValue(userJson, User.class);
         return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
     @GetMapping("/get")
-    public ResponseEntity<List<User>> getUsuer() throws JsonProcessingException {
-        RemoteCache<String, String> cache = cacheManager.getCache("usuarios");
+    public ResponseEntity<List<User>> getAllUsuers() throws JsonProcessingException {
         List<User> users = new ArrayList<>();
         for (Map.Entry<String, String> entry : cache.entrySet()) {
+            log.debug("Fetch user: {}", entry.getValue());
             users.add(objectMapper.readValue(entry.getValue(), User.class));
         }
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
 
+    @PutMapping("/update")
+    public ResponseEntity<Void> updateUser(@RequestBody User user) throws JsonProcessingException {
+        if (cache.containsKey(user.getId())){
+            String userJson = objectMapper.writeValueAsString(user);
+            cache.put(user.getId(), userJson);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 
 }
