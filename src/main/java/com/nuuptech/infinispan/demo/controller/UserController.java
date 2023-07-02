394f5bd6659e0cf1d7cc72ba287257eb928847ba
Oscar <oscar.cn1@gmail.com>
2023-07-02 07:50:31 -0600

    Cache entry creation

--- src/main/java/com/nuuptech/infinispan/demo/controller/UserController.java
+++ src/main/java/com/nuuptech/infinispan/demo/controller/UserController.java
@@ -9,10 +9,7 @@ import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -28,12 +25,20 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Void> crearUsuario(@RequestBody User user) throws JsonProcessingException {
+    public ResponseEntity<Void> createUser(@RequestBody User user) throws JsonProcessingException {
         RemoteCache<String, String> cache = cacheManager.getCache("usuarios");
         String userJson = objectMapper.writeValueAsString(user);
         log.info("User: {}", userJson);
         cache.put(user.getId(), userJson);
         return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<User> getUsuer(@PathVariable String id) throws JsonProcessingException {
+        RemoteCache<String, String> cache = cacheManager.getCache("usuarios");
+        String userJson = cache.get(id);
+        User user = objectMapper.readValue(userJson, User.class);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
 }
