846321536af09b62ea402c316fe591e8f871e59f
Oscar <oscar.cn1@gmail.com>
2023-07-02 05:30:24 -0600

    Initial import

---
+++ src/main/java/com/nuuptech/infinispan/demo/InfinispanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nuuptech.infinispan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinispanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinispanDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuuptech/infinispan/demo/InfinispanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuuptech.infinispan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinispanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
